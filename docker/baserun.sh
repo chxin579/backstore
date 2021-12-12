8dd960a58d56f5498259ba9b38b7d17e985d6145
chxin579 <43876662+chxin579@users.noreply.github.com>
2021-12-12 09:08:28 +0800

    fix some display

--- docker/baserun.sh
+++ docker/baserun.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-echo "更新OneShareUpload"
-wget http://file.thiinkget.eu.org/uploads/LinuxX64/OneShareUpload -O /usr/local/bin/OneShareUpload && chmod +x /usr/local/bin/OneShareUpload
+echo "更新Update OneShareUpload"
+wget -q http://file.thiinkget.eu.org/uploads/LinuxX64/OneShareUpload -O /usr/local/bin/OneShareUpload && chmod +x /usr/local/bin/OneShareUpload
 
 OneShareUpload -n
 
-echo "更新mv.sh"
-wget https://raw.githubusercontent.com/chxin579/backstore/master/docker/mv.sh -O /root/mv.sh && chmod +x /root/mv.sh
+echo "更新Update mv.sh"
+wget -q https://raw.githubusercontent.com/chxin579/backstore/master/docker/mv.sh -O /root/mv.sh && chmod +x /root/mv.sh
 
-echo "更新完毕"
+echo "更新完毕Update Finish"
